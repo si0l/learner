3980483810a3db7f02e5659a634e33be1429614c
si0l <psuboch@gmail.com>
2022-10-18 20:38:01 +0300

    genrated spring skeleton, added initial dockerfile

---
+++ learner/src/test/java/com/sub/learner/LearnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sub.learner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
